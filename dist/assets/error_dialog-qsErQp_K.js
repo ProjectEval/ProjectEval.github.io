0279551457d47f107b9da2417331af7aa150a089
Nathaniel Kemme Nash <nkemme54@gmail.com>
2024-01-05 16:03:20 -0500

    After trip changes

--- dist/assets/error_dialog-qsErQp_K.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./auth-OIc4apUQ.js";function t({error:e,errorTitle:c,errorModalRef:i,onClose:n}){return r.jsx(r.Fragment,{children:r.jsxs("dialog",{className:"errorSignUp",ref:i,children:[r.jsx("h2",{children:c}),r.jsx("p",{children:e}),r.jsx("button",{onClick:()=>{var s;(s=i.current)==null||s.close(),n!=null&&n()},children:"Close"})]})})}export{t as E};
---
+++ dist/assets/info_dialog-csbS-ued.js
@@ -0,0 +1,1 @@
+import{j as r}from"./auth-x3Nz2WY_.js";function l({info:e,Title:o,infoModalRef:i,onClose:n}){return r.jsx(r.Fragment,{children:r.jsxs("dialog",{className:"infoDialog",ref:i,children:[r.jsx("h2",{children:o}),r.jsx("p",{children:e}),r.jsx("button",{onClick:()=>{var s;(s=i.current)==null||s.close(),n!=null&&n()},children:"Close"})]})})}export{l as I};
