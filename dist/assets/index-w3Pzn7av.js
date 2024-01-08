d2075a3dba1b2a8dc1ed1b05c998752fb236a7f4
Nathaniel Kemme Nash <nkemme54@gmail.com>
2024-01-08 10:47:09 -0500

    Fixed Coloring

---
+++ dist/assets/index-w3Pzn7av.js
@@ -0,0 +1,1 @@
+import{c as e,j as o,R as t,g as i}from"./auth-suFFnFMY.js";import{D as n}from"./dashboard-uZHhOqlz.js";async function a(){await i()||(window.location.href=window.location.origin+"/Login/")}(async()=>await a())();e.createRoot(document.getElementById("root")).render(o.jsx(t.StrictMode,{children:o.jsx(n,{})}));
