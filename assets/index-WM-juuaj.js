e5aa0fa01ae90094a9b51fc7c8808ce3ac4beab1
Nathaniel Kemme Nash <nkemme54@gmail.com>
2024-01-08 11:31:11 -0500

    Fixed Groups

---
+++ assets/index-WM-juuaj.js
@@ -0,0 +1,1 @@
+import{c as e,j as o,R as t,g as i}from"./auth-7a6rBG4F.js";import{D as n}from"./dashboard-rW8i4MVE.js";async function a(){await i()||(window.location.href=window.location.origin+"/Login/")}(async()=>await a())();e.createRoot(document.getElementById("root")).render(o.jsx(t.StrictMode,{children:o.jsx(n,{})}));
