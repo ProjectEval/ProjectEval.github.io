1b2b511e37aff6067d4be26cbcb60363324f8903
Nathaniel Kemme Nash <nkemme54@gmail.com>
2024-01-08 10:40:07 -0500

    Changed to FB

--- dist/assets/dashboard-2b10FecQ.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as o,R as t,g as i}from"./auth-x3Nz2WY_.js";import{D as n}from"./dashboard-2Psm1ZJk.js";async function a(){await i()||(window.location.href=window.location.origin+"/Login/")}(async()=>await a())();e.createRoot(document.getElementById("root")).render(o.jsx(t.StrictMode,{children:o.jsx(n,{})}));
