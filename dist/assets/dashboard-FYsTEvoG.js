ef2074ab357bcafd826212ecd49088ba65f7f907
Nathaniel Kemme Nash <nkemme54@gmail.com>
2024-01-08 10:54:28 -0500

    Fixed Button Coloring

---
+++ dist/assets/dashboard-FYsTEvoG.js
@@ -0,0 +1,1 @@
+import{c as e,j as o,R as t,g as i}from"./auth-Hj7L4AiZ.js";import{D as n}from"./dashboard-BxFjeSMB.js";async function a(){await i()||(window.location.href=window.location.origin+"/Login/")}(async()=>await a())();e.createRoot(document.getElementById("root")).render(o.jsx(t.StrictMode,{children:o.jsx(n,{})}));
