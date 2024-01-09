79455dba6b7f45b2c0f47ee351e4bccd24b26acd
Nathaniel Kemme Nash <nkemme54@gmail.com>
2024-01-08 21:02:53 -0500

    Fixed Scrollbars

--- assets/info_dialog-q-ePwUyF.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./auth-7a6rBG4F.js";function l({info:e,Title:o,infoModalRef:i,onClose:n}){return r.jsx(r.Fragment,{children:r.jsxs("dialog",{className:"infoDialog",ref:i,children:[r.jsx("h2",{children:o}),r.jsx("p",{children:e}),r.jsx("button",{onClick:()=>{var s;(s=i.current)==null||s.close(),n!=null&&n()},children:"Close"})]})})}export{l as I};
